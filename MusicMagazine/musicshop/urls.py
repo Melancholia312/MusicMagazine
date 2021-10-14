3b2e6d2c5002b8b0aeb8c22ba9784d5e09a70dae
Melancholia312 <enot_yanovskiy@mail.ru>
2021-10-15 01:18:42 +0300

    'first_update'

--- MusicMagazine/musicshop/urls.py
+++ MusicMagazine/musicshop/urls.py
@@ -1,8 +1,10 @@
 from django.urls import path
-from .views import BaseView, AlbumDetailView, ArtistDetailView
+from .views import BaseView, AlbumDetailView, ArtistDetailView, RegistrationView, LoginView
 
 urlpatterns = [
+    path('login/', LoginView.as_view(), name='login'),
+    path('registration/', RegistrationView.as_view(), name='registration'),
     path('', BaseView.as_view(), name='base'),
     path('<str:artist_slug>/', ArtistDetailView.as_view(), name='artist_detail'),
-    path('<str:artist_slug>/<str:album_slug>/', AlbumDetailView.as_view(), name='album_detail')
+    path('<str:artist_slug>/<str:album_slug>/', AlbumDetailView.as_view(), name='album_detail'),
 ]
